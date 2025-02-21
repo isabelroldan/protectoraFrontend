ef8c884a0c60bdea1df0439486d44976ca8a8daa
Isabel Roldán Córdoba <isabelroldancordoba@hotmail.com>
2025-02-21 19:44:21 +0100

    Commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Router from './Components/Router'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+
+  return (
+    <>
+     <Router></Router>
+    </>
+  )
+}
+
+export default App
