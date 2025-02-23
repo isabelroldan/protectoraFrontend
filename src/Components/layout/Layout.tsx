9d9f2e275788abdcf16b232a533e472d9a2635ca
Isabel Roldán Córdoba <isabelroldancordoba@hotmail.com>
2025-02-23 10:05:09 +0100

    Añadida comprobación de usuario logueado para redirigir al login en caso de no estarlo

--- src/Components/layout/Layout.tsx
+++ src/Components/layout/Layout.tsx
@@ -1,7 +1,17 @@
 import LayoutNav from './Nav/LayoutNav';
 import LayoutFooter from './Footer/LayoutFooter';
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 function Layout({ children }: { children: React.ReactNode }) {
+    const token = useSelector((state: any) => state.login.token);
+    const navigateTo = useNavigate()
+
+    useEffect(() => {
+        if (!token) navigateTo('/login')
+    }, [navigateTo, token])
+
     return (
         <>
             <LayoutNav />
