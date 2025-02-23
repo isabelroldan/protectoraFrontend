32f092249c5540f5ada0e8c21fad0b81cfdbdc48
Isabel Roldán Córdoba <isabelroldancordoba@hotmail.com>
2025-02-23 10:05:26 +0100

    Actualizada la store para añadir mascotas y usuarios

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import loginReducer from '../Components/login/loginSlice'
+import mascotasReducer from '../Components/Mascotas/mascotasSlice'
+import usuariosReducer from '../Components/Usuarios/usuariosSlice'
 export default configureStore({
   reducer: {
-    login: loginReducer
+    login: loginReducer,
+    mascotas: mascotasReducer,
+    usuarios: usuariosReducer
   },
 })
