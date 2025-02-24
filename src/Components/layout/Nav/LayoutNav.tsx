010dfffff15a931e2c3a02ef0da0bff4c236f26e
Isabel Roldán Córdoba <isabelroldancordoba@hotmail.com>
2025-02-24 21:56:01 +0100

    Aplicado los estilos para el nav

--- src/Components/layout/Nav/LayoutNav.tsx
+++ src/Components/layout/Nav/LayoutNav.tsx
@@ -1,4 +1,4 @@
-import { Link, useNavigate } from 'react-router-dom';
+/*import { Link, useNavigate } from 'react-router-dom';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { useState } from 'react';
 import { logoutAsync } from '../../login/loginSlice';
@@ -34,4 +34,63 @@ function LayoutNav() {
     )
 }
 
-export default LayoutNav
+export default LayoutNav*/
+
+
+import React, { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { logoutAsync } from '../../login/loginSlice';
+import styles from './LayoutNav.module.css';
+import { ThunkDispatch } from '@reduxjs/toolkit';
+
+function LayoutNav() {
+    const dispatch = useDispatch<ThunkDispatch<any, any, any>>()
+
+    const [token, setToken] = useState(sessionStorage.getItem("token"));
+
+    const handleLogout = () => {
+        dispatch(logoutAsync())
+    };
+
+    return (
+        <nav className={styles.navBar}>
+            <div className={styles.logo}>Pelitos y Bigotes</div>
+            <ul className={styles.navList}>
+                <li className={styles.navItem}>
+                    <Link
+                        to="/mascotas"
+                        className={location.pathname === '/mascotas' ? styles.active : ''}
+                    >
+                        Mascotas
+                    </Link>
+                </li>
+                <li className={styles.navItem}>
+                    <Link
+                        to="/solicitudes"
+                        className={location.pathname === '/solicitudes' ? styles.active : ''}
+                    >
+                        Solicitudes
+                    </Link>
+                </li>
+                <li className={styles.navItem}>
+                    <Link
+                        to="/usuarios"
+                        className={location.pathname === '/usuarios' ? styles.active : ''}
+                    >
+                        Usuarios
+                    </Link>
+                </li>
+
+                {token && (
+                    <li className={styles.navItem}>
+                        <a href="#" onClick={handleLogout}>Logout</a>
+                    </li>
+                )}
+            </ul>
+        </nav>
+    );
+}
+
+export default LayoutNav;
+
