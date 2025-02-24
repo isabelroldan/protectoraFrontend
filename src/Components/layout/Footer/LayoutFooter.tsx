675a84a321a8577cc1d777b3750cd94ac6b28027
Isabel Roldán Córdoba <isabelroldancordoba@hotmail.com>
2025-02-24 21:55:29 +0100

    Aplicacion de eestilo para el Pie

--- src/Components/layout/Footer/LayoutFooter.tsx
+++ src/Components/layout/Footer/LayoutFooter.tsx
@@ -1,4 +1,4 @@
-import { Col, Container, Image, Nav, NavLink, Row, Stack } from "react-bootstrap"
+/*import { Col, Container, Image, Nav, NavLink, Row, Stack } from "react-bootstrap"
 
 function LayoutFooter() {
 
@@ -41,3 +41,46 @@ function LayoutFooter() {
 }
 
 export default LayoutFooter
+*/
+
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import styles from './LayoutFooter.module.css';
+
+function LayoutFooter() {
+    return (
+        <footer className={styles.footer}>
+            <Container>
+                <Row>
+                    <Col md={4}>
+                        <h3 className={styles.footerTitle}>Protectora de Animales</h3>
+                        <p>Dedicados al cuidado y adopción de mascotas desde 2010.</p>
+                    </Col>
+                    <Col md={4}>
+                        <h4 className={styles.footerSubtitle}>Enlaces Útiles</h4>
+                        <ul className={styles.footerLinks}>
+                            <li><Link to="/">Inicio</Link></li>
+                            <li><Link to="/mascotas">Mascotas en Adopción</Link></li>
+                            <li><Link to="/solicitudes">Solicitar Adopción</Link></li>
+                            <li><Link to="/about">Sobre Nosotros</Link></li>
+                        </ul>
+                    </Col>
+                    <Col md={4}>
+                        <h4 className={styles.footerSubtitle}>Contáctanos</h4>
+                        <p>Email: adopciones@protectora.com</p>
+                        <p>Teléfono: 123-456-7890</p>
+                        <p>Dirección: Calle Mascota 123, Ciudad</p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className={styles.footerBottom}>
+                        <p>&copy; 2023 Protectora de Animales. Todos los derechos reservados.</p>
+                    </Col>
+                </Row>
+            </Container>
+        </footer>
+    );
+}
+
+export default LayoutFooter;
